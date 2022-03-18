7c124281bc04ca8ad4cc8a252108b45eacad2dc3
SunCreation <cake0103@nate.com>
2022-03-18 21:31:24 +0900

    hchang

--- interview/06_string_manipulation/hchang/mostcommon.py
+++ interview/06_string_manipulation/hchang/mostcommon.py
@@ -1,5 +1,5 @@
 """ 
-급지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 대소문자를 구분하지
+금지된 단어를 제외한 가장 흔하게 등장하는 단어를 출력하라. 대소문자를 구분하지
 않으며, 구두점 또한무시한다 """
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = {"hit"}
