5555470d181b59fa026cf1f90682db5806a7a797
SunCreation <cake0103@nate.com>
2022-05-09 07:40:59 +0900

    hchang

--- interview/12_graph/hchang/39.py
+++ interview/12_graph/hchang/39.py
@@ -1,3 +1,12 @@
-N = 2
-corse = [[1,0],[0,1]]
+N = 7
+corse = [[1,0],[3,6],[4,2],[3,6],[6,0],[7,1]]
 
+class Solution:
+    def check(self, N, corse):
+        ...
+
+
+sol = Solution()
+
+print(sol.check(N,corse))
+        
